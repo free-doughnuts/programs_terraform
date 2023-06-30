88ea93e8f3d452422a77a74f1c545854ec858019
free-doughnuts <kc.sudoer@gmail.com>
2023-06-30 12:15:04 +0300

    update-remote

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,1 +1,19 @@
+variable "region" {
+  type = string
+}
 
+variable "instance_type" {
+  type = string
+}
+
+variable "ami_type" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
