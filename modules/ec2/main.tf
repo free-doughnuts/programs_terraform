de4697ab88fea69f9526f38fdabdeaa7c543cbe5
free-doughnuts <kc.sudoer@gmail.com>
2023-07-01 23:35:16 +0300

    update-remote-code

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -22,6 +22,7 @@ resource "aws_instance" "ec2" {
   instance_type          = var.instance_type
   ami                    = var.ami_type
   subnet_id              = var.subnet_id
+  aws_eip                = var.aws_eip
   tags = {
     Name = var.name
   }
