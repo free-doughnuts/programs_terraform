b2aaf1b3cc633248287a3dfc100b787eb4c8e589
free-doughnuts <kc.sudoer@gmail.com>
2023-07-04 23:09:39 +0300

    status = added

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -1,14 +1,35 @@
+ // the technology
+ 
+ terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.13.0"
+    }
+  }
+
+}
+
 // the provider
  
  provider "aws" {
   region = var.region
  }
  
- // the resource : s3 bucket
+ // the resource: bucket
  
- resource "aws_s3_bucket" "first-bucket" {
-   acl                = var.acl
+ resource "aws_s3_bucket" "via-terraform" {
+   bucket             = var.bucket
    tags = {
      Name             = var.name
    }
  }
+
+// the bucket configuration
+
+resource "aws_s3_bucket_versioning" "via-terraform" {
+  bucket              = var.bucket
+  versioning_configuration {
+    status            = var.versioning
+  }
+}
---
+++ modules/s3-bucket/punchvars.tfvars
@@ -0,0 +1,7 @@
+region            = "add the region here (e.g: eu-central-1)"
+
+bucket            = "add the bucket name here (e.g: first-bucket)"
+
+name              = "add the bucket tag here (e.g: Name = tf-bucket)"
+
+versioning        = "set versioning stauts (e.g: "Enabled")
