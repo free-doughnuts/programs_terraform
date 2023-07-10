56ed6a6617b3123c04c4f2fdf98664f62d78365b
free-doughnuts <kc.sudoer@gmail.com>
2023-07-10 23:35:46 +0300

    status = added

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -30,7 +30,3 @@ resource "aws_instance" "ec2" {
 resource "aws_eip" "elastic_ip_addr" {
   instance               = aws_instance.ec2.id
 }
-
-output "MACHINE_EIP_IS" {
-  value = aws_eip.elastic_ip_addr.public_ip
-}
